ab7a0f46e885a987446315a3a14d3c46601589d2
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-07-19 20:29:04 -0700

    Added food (chocolate, why not?)

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import pygame
+import pygame, time
 from pygame.locals import *
-import time
 
 BLOCK_SIZE = 40
+
+class Chocolate:         
+    def __init__(self, surface):
+        self.chocolate = pygame.image.load('src/chocolate.png')
+        self.surface = surface
+        self.x = 120
+        self.y = 120
+
+    def draw_chocolate(self):
+        self.surface.blit(self.chocolate,(self.x, self.y))
+        pygame.display.flip()
+
 
 class Snake:
     def __init__(self, surface, length):
         self.length = length
         self.parent_screen = surface
-        self.block = pygame.image.load("src/block.jpg").convert() 
+        self.block = pygame.image.load('src/block.jpg').convert() 
         self.x = [BLOCK_SIZE]*length
         self.y = [BLOCK_SIZE]*length
         self.direction = 'down'
@@ -25,7 +36,7 @@ class Snake:
     def move_down(self):
         self.direction = 'down'
 
-    def draw(self):
+    def draw_length(self):
         self.parent_screen.fill((0, 102, 51))
         for i in range(self.length):
             self.parent_screen.blit(self.block,(self.x[i], self.y[i]))
@@ -38,7 +49,6 @@ class Snake:
             self.x[i] = self.x[i -1]
             self.y[i] = self.y[i -1]
 
-
         if self.direction == 'left':
             self.x[0] -= BLOCK_SIZE
         if self.direction == 'right':
@@ -48,7 +58,7 @@ class Snake:
         if self.direction == 'down':
             self.y[0] += BLOCK_SIZE
 
-        self.draw()
+        self.draw_length()
 
 
 class Game:
@@ -56,7 +66,13 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((900, 500))
         self.snake = Snake(self.surface, 3)
-        self.snake.draw()
+        self.snake.draw_length()
+        self.chocolate = Chocolate(self.surface)
+        self.chocolate.draw_chocolate()
+
+    def play(self):
+        self.snake.glides()
+        self.chocolate.draw_chocolate()
 
     def run(self):
         running = True
@@ -66,22 +82,24 @@ class Game:
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
-                    if event.key == K_LEFT:
+                    if event.key == K_LEFT or event.key == ord('a'):
                         self.snake.move_left()
 
-                    if event.key == K_RIGHT:
+                    if event.key == K_RIGHT or event.key == ord('d'):
                         self.snake.move_right()
 
-                    if event.key == K_UP:
+                    if event.key == K_UP or event.key == ord('w'):
                         self.snake.move_up()
 
-                    if event.key == K_DOWN:
+                    if event.key == K_DOWN or event.key == ord('s'):
                         self.snake.move_down()
 
                 elif event.type == QUIT:
                     running = False
-            self.snake.glides()
-            time.sleep(0.2)
+                    
+            self.play()
+
+            time.sleep(0.3)
 
 if __name__ == '__main__':
     game = Game()
